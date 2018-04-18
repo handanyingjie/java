304f2bbb18008581a1e0b8605e0b14b59e97fbff
刘英杰 <handanyingjie@sina.com>
2018-04-18 21:14:15 +0800

    若要产生[a,b]之间的随机数，其通式为(b - a + 1) * Math.random() + a

--- ep8_2.java
+++ ep8_2.java
@@ -1,11 +1,11 @@
 //产生10个10-100之间的随机整数。
+//若要产生[a,b]之间的随机数其通式为:(b - a + 1) * Math.random() + a
 public class ep8_2 {
     public static void main(String args[]){
         int a;
         System.out.print("随机数为:");
         for (int i = 1; i <= 10; i++){
-//            a = (int) ((100 - 10 + 1)* Math.random() + 10);
-            a = (int) (99 * Math.random() + 10);
+            a = (int) ((100 - 10 + 1)* Math.random() + 10);
             System.out.print(" " + a);
         }
         System.out.println();
