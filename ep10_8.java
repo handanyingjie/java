e0eff7bc96da042024117cb311317fede3f2725d
刘英杰 <handanyingjie@sina.com>
2018-04-18 15:32:23 +0800

    读取数据文件ep10_7.dat 中的三个 int 型数字，求和并显示

---
+++ ep10_8.java
@@ -0,0 +1,20 @@
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+//读取数据文件ep10_7.dat 中的三个 int 型数字，求和并显示
+public class ep10_8 {
+    public static void main(String args[]){
+        String fileName = "ep10_7.data";
+        int sum = 0;
+        try{
+            DataInputStream a = new DataInputStream(new BufferedInputStream(new FileInputStream(fileName)));
+            sum += a.readInt();
+            sum += a.readInt();
+            sum += a.readInt();
+            System.out.println("三个数的和为:" + sum);
+            a.close();
+        }catch (IOException e){}
+    }
+}
