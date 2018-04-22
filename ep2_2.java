8a82feaf9811dd6a2f500df538b6dd96e222cdee
刘英杰 <handanyingjie@sina.com>
2018-04-22 21:31:41 +0800

    日历

---
+++ ep2_2.java
@@ -0,0 +1,10 @@
+import java.io.FileNotFoundException;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class ep2_2 {
+    public static void main(String args[]) throws FileNotFoundException{
+        Scanner s = new Scanner("ep10_12.log");
+        System.out.println(s);
+    }
+}
